50ed1524d5c5ee067fa534f21245628380d43c96
Spad0n <92205208+Spad0n@users.noreply.github.com>
2025-10-05 14:33:47 +0200

    code minimal avec express

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 6969;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/hello', (_req, res) => {
+    res.json("Hello from json");
+})
+
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.listen(PORT, (_err) => {
+    console.log(`Serveur lancé sur https://localhost:${PORT}`);
+});

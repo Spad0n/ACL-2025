aab6ab4ab91a4de4102fb39cbeb2ca84a328fc2f
INSTABLE <leo.andre.fr.907@gmail.com>
2025-11-18 17:06:28 +0100

    création automatique du dossier pour les agendas JSON

--- initialisation.js
+++ initialisation.js
@@ -3,21 +3,21 @@
  */
 "use strict";
 
-import { writeFile, existsSync } from 'fs';
+import { writeFile, existsSync, mkdirSync } from 'fs';
 
 function creationFichierBDD() {
 
     const fileName = `bdd.db`;
     if (existsSync(fileName)) { 
-	console.log(`Le fichier : ${fileName} existe déjà !`);
+        console.log(`Le fichier : ${fileName} existe déjà !`);
     }
     else {
-	writeFile(fileName, "", (err) => {
-	    if (err) {
-		console.error('ERREUR : creationFichierBDD()', err);
-	    }
-	    console.log(`Le fichier : ${fileName} a été créé !`);
-	});
+        writeFile(fileName, "", (err) => {
+                if (err) {
+                console.error('ERREUR : creationFichierBDD()', err);
+                }
+                console.log(`Le fichier : ${fileName} a été créé !`);
+                });
     }
 }
 
@@ -25,21 +25,32 @@ function creationFichierDotENV() {
     const fileName = `.env`;
 
     if(existsSync(fileName)) {
-	console.log(`Le fichier : ${fileName} existe déjà !`);
+        console.log(`Le fichier : ${fileName} existe déjà !`);
     }
     else {
-	writeFile(fileName, "SECRET=fe3f488af30d66d7fee0e2d13f43c25a036b87541987b099bb9d036b3a12d4366866af43dd0497895ad776810097f655bd73c52b8e926e3d8a2b455e88b9d79a", (err) => {
-	    if(err) {
-		console.error(`ERREUR : creationFichierDotENV()`, err);
-	    }
-	    console.log(`Le fichier ${fileName} a été créé !`);
-	});
+        writeFile(fileName, "SECRET=fe3f488af30d66d7fee0e2d13f43c25a036b87541987b099bb9d036b3a12d4366866af43dd0497895ad776810097f655bd73c52b8e926e3d8a2b455e88b9d79a", (err) => {
+                if(err) {
+                console.error(`ERREUR : creationFichierDotENV()`, err);
+                }
+                console.log(`Le fichier ${fileName} a été créé !`);
+                });
+    }
+}
+
+function creationDossierAgendasJson() {
+    const directoryPath = './server/agendaJson';
+    if (!existsSync(directoryPath)) {
+        mkdirSync(directoryPath);
+        console.log(`Le dossier ${directoryPath} a été créé !`);
+    } else {
+        console.log(`Le dossier ${directoryPath} existe déjà !`);
     }
 }
 
 function init() {
     creationFichierBDD();
     creationFichierDotENV();
+    creationDossierAgendasJson();
 }
 
 init();
